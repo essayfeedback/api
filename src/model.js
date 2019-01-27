1424ccdbbdfb6cbea5ede19d845550c1a8bbf94e
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-01-27 08:20:16 -0800

    checkpoint

--- src/model.js
+++ src/model.js
@@ -5,8 +5,7 @@ const model = new schema({
   areas: [Boolean],
   stage: String,
   question: String,
-  link: String,
-  customArea: String
+  link: String
 });
 
 module.exports = mongoose.model("Essay", model);
