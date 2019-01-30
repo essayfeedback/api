1177d564752cf92c4cba463ca38569829d090b14
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-01-30 11:41:38 -0800

    added user model schema

--- src/model.js
+++ src/model.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const model = new schema({
-  areas: [Boolean],
-  stage: String,
+const essayModel = new schema({
+  selectedAreas: [Boolean],
+  selectedStage: Number,
   question: String,
-  link: String
+  link: String,
+  ownerUID: String,
+  reviewerUID: String,
+  status: Boolean
 });
 
-module.exports = mongoose.model("Essay", model);
+const userModel = new schema({
+  numPosted: Number,
+  numReviewed: Number,
+  rating: Number,
+  uid: String
+});
+
+module.exports = mongoose.model("Essay", essayModel);
