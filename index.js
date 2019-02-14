d4c4234eb6cd02a232235cf6c81da08a6f51dcca
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-02-13 17:17:27 -0800

    added mongoose event handlers

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const {
 } = require("./secrets.json");
 
 mongoose.connect(`mongodb://${username}:${encodeURIComponent(password)}@ds161804.mlab.com:61804/essayfeedback`, { useNewUrlParser: true });
-mongoose.set("debug", true);
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,4 +18,24 @@ app.use("/api/users", require("./src/routes/users"));
 app.listen(PORT, err => {
   if (err) throw err;
   console.log(`server ready at http://localhost:${PORT}`);
+});
+
+mongoose.set("debug", true);
+mongoose.connection.on("connected", function() {
+  console.log("Mongoose connection open to " + dbURI);
+});
+
+mongoose.connection.on("error", function(err) {
+  console.log("Mongoose connection error: " + err);
+});
+
+mongoose.connection.on("disconnected", function() {
+  console.log("Mongoose connection disconnected");
+});
+
+process.on("SIGINT", function() {
+  mongoose.connection.close(function() {
+    console.log("Mongoose connection disconnected through app termination");
+    process.exit(0);
+  });
 });
